645a1d53933e0b2acdb9dd152e378453e7c25134
Steven Wang <stwa@itu.dk>
2023-03-30 15:51:15 +0200

    added hud arrow

--- linked.py
+++ linked.py
@@ -19,11 +19,17 @@ class LinkedList(Scene):
 
 class multiLink(MovingCameraScene):
     def construct(self):
+        sq = Arrow()
+        arrowname = Text("The Allmighty Finger of God")
+        arrowname.move_to((0,3.5,0))
+        self.add(arrowname)
+        sq.put_start_and_end_on((0,3,0),(0,1.5,0))
+        self.add(sq)
         t = Text("0")
         mob = LinkedListNode(t)
         self.play(Create(mob))
         center = mob.get_center()
-
+        cam = VGroup(self.camera.frame,sq,arrowname)
         for x in range(4):
             y = x+1
             name = str(y)
@@ -31,14 +37,14 @@ class multiLink(MovingCameraScene):
             boxy.move_to(center)
             boxy.shift(UP*10+RIGHT*3*y)
             #self.play(mob.animate(run_time=y/4).shift(LEFT*3*y))
-            self.play(self.camera.frame.animate(run_time=y/4).move_to((3*y,0,0)))
+            self.play(cam.animate(run_time=y/4).move_to((3*y,0,0)))
             self.play(boxy.animate.shift(DOWN*10))
             anim = mob.add_node(Text(name))
             self.add(anim[0])
             self.remove(boxy)
             self.play(Create(anim[1]))
             #self.play(mob.animate(run_time = 0.25).shift(RIGHT*3*y))
-            self.play(self.camera.frame.animate(run_time = 0.25).move_to((0,0,0)))
+            self.play(cam.animate(run_time = 0.25).move_to((0,0,0)))
             self.play(Wait(run_time=0.25))
 
         for z in range(50):
@@ -52,7 +58,7 @@ class multiLink(MovingCameraScene):
         boxy.move_to(center)
         boxy.shift(UP*10+RIGHT*3*y)
         #self.play(mob.animate(run_time=y/4).shift(LEFT*3*y))
-        self.play(self.camera.frame.animate(run_time=y/10).move_to((3*y,0,0)))
+        self.play(cam.animate(run_time=y/10).move_to((3*y,0,0)))
         self.play(boxy.animate.shift(DOWN*10))
         anim = mob.add_node(Text(name))
         self.add(anim[0])
