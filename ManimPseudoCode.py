e95556e00ce8facedb0e668b593e2bfcf7bdb47f
Steven Wang <stwa@itu.dk>
2023-04-20 13:03:44 +0200

    Added textwithboundingbox class

--- ManimPseudoCode.py
+++ ManimPseudoCode.py
@@ -47,3 +47,29 @@ class PseudoCode(VGroup):
                 return r
             r.shift(DOWN*0.2118770857773395*height)
         return r
+class TextWithBoundingBox(VGroup):
+    def __init__(self, text, fontsize):
+        super().__init__()
+        self.text = Text(text, font_size= fontsize)
+        self.box = get_box(self.text,fontsize)
+        self.add(self.text,self.box)
+
+
+
+
+def get_box(self,mob, fontsize : float = DEFAULT_FONT_SIZE): 
+    height = Text("fg", font_size=fontsize)
+    padding = 0.0 #might make this a parameter
+    r = Rectangle()
+    r.stroke_width = 0
+    r.stretch_to_fit_width(mob.width+padding)
+    r.move_to(mob)
+    r.stretch_to_fit_height((self.height+padding))
+    r.align_to(mob,DOWN)
+    r.shift(DOWN*(padding/2))
+    diffp = (height-mob.height)/height
+    print(diffp)
+    if diffp == 0 or (diffp >= 0.23 and diffp <= 0.24):
+        return r
+    r.shift(DOWN*0.2118770857773395*height)
+    return r
