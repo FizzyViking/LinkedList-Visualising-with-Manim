df19abe6150a3b05dc925892f3ef21d9f78331c6
alexander.rode2 <alexander.rode2@gmail.com>
2023-04-21 12:56:59 +0200

    rapportexs, add error handling to pseudo class

--- ManimPseudoCode.py
+++ ManimPseudoCode.py
@@ -16,34 +16,40 @@ class PseudoCode(VGroup):
         self.code_file = code_file
         self.lines = []
         self.font_size = font_size
+        self.height = Text("fg",font_size=self.font_size).height
+        self.line_space = 2
 
         self.code_string = None
-        self.file_path = Path(self.code_file)
-        if code_file:
-            # check_valid_path()
+        if self.code_file:
+            self.file_path = self.isValidPath(self.code_file)
             self.code_string = self.file_path.read_text(encoding="utf-8")
-        else:
+        elif self.code:
             self.code_string = self.code
+        else:
+            raise ValueError("No file or code was given")
     
     def get_codestring(self):
         return self.code_string
     
+    def isValidPath(self, file_name):
+        if file_name is None:
+            raise ValueError("File of code is not properly defined")
+        return Path(file_name)
 
     """ Mark up to n lines """
     def marklines(self, line_numbers):
         for line in line_numbers:
             # mob = get_line(line)
-            height = Text("fg",font_size=self.font_size).height
             padding = 0.1 
             r = Rectangle()
             r.stretch_to_fit_width(mob.width+padding)
             r.move_to(mob)
-            r.stretch_to_fit_height((height+padding))
+            r.stretch_to_fit_height((self.height+padding))
             r.align_to(mob,DOWN)
             r.shift(DOWN*(padding/2))
-            diffp = (height-mob.height) / height
+            diffp = (self.height-mob.height) / self.height
             print(diffp)
             if diffp == 0 or (diffp >= 0.23 and diffp <= 0.24):
                 return r
-            r.shift(DOWN*0.2118770857773395*height)
+            r.shift(DOWN*0.2118770857773395*self.height)
         return r
