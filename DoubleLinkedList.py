b979f7cdd599c2f22671f7b4549a1b55bf4e0ff4
alexander.rode2 <alexander.rode2@gmail.com>
2023-04-27 13:59:25 +0200

    update doublelinked example py file

--- DoubleLinkedList.py
+++ DoubleLinkedList.py
@@ -1,6 +1,14 @@
 class DoubleLinkedList():
-    def __init__(self):
+    def __init__(self, data):
         self.next = None
         self.prev = None
-        self.data = None
-        self.gaga = None
+        self.data = data
+    def insert_middle(self, prev_node, data):
+        if prev_node is None:
+            return
+        new_node = DoubleLinkedList(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+        new_node.prev = prev_node
+        if new_node.next is not None:
+            new_node.next.prev = new_node
